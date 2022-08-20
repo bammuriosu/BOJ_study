cf47cb06c4b8cf5ad5cdcb7407998f6a440901c0
bammuri <bammuriosu@gmail.com>
2022-08-20 18:20:04 +0900

    qmq

--- 2022_08/4-4게임개발.py
+++ 2022_08/4-4게임개발.py
@@ -1,1 +1,10 @@
-print(3)
+#첫째줄에 맵의 세로크기 n 가로크기 m을 공백으로 구분하여 입력한다.
+n, m = map(int, input().split())
+
+#방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
+d = [[0] * m for _ in range(n)]
+#현재 캐릭터의 X좌표, Y좌표, 방향을 입력받기
+x,y,direction = map(int,input().split())
+d[x][y] = 1 #현재 좌표 방문 처리
+
+#
---
+++ 99-1.새싹.py
@@ -0,0 +1,10 @@
+def solve():
+    print("         ,r'\"7") #"을 치기 위해 \"으로 표현함
+    print("r`-_   ,'  ,/")
+    print(" \\. \". L_r'")#\를 치기 위해 \\로 표현함
+    print("   `~\\/")
+    print("      |")
+    print("      |")
+
+if __name__ == '__main__': #메인함수 시작
+    solve() #함수가동
