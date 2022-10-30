c8e8262a792e7a95e5ac6b1fe837ddf3fcd89783
bammuri <bammuriosu@gmail.com>
2022-10-30 17:20:15 +0900

    10.30

--- 2022_10/2738_행렬덧셈.py
+++ 2022_10/2738_행렬덧셈.py
@@ -3,4 +3,10 @@
 #이어서 N개의 줄에 행렬 B의 원소 M개가 차례대로 주어진다.
 # N과 M은 100보다 작거나 같고, 행렬의 원소는 절댓값이 100보다 작거나 같은 정수이다.
 
-N, M = map(int, input().split())
+N, M = map(int, input().split()) 
+a = [list(map(int, input().split())) for _ in range(N)] #행렬a 입력받기
+for i in range(N) :
+    b = list(map(int, input().split())) #행렬b 입력받기
+    for j in range(M):
+        print(a[i][j]+b[j], end=' ') #합치고 스페이스바
+    print()# 프린트
