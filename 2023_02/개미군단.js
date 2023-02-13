5e9ce95ea6d975d11c328f9ad21abe31e24f7141
nowaveosu <nowaveosu@gmail.com>
2023-02-13 19:41:05 +0900

    2.13

--- 2023_02/개미군단.js
+++ 2023_02/개미군단.js
@@ -23,8 +23,8 @@ function solution(hp) {
 }
 //추천풀이
 function solution(hp) {
-  const first = Math.floor(hp / 5);
-  const second = Math.floor((hp - first * 5) / 3);
-  const third = hp - first * 5 - second * 3;
-  return first + second + third;
+    const first = Math.floor(hp / 5);
+    const second = Math.floor((hp - first * 5) / 3);
+    const third = hp - first * 5 - second * 3;
+    return first + second + third;
 }
