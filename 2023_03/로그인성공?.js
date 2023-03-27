8c927f9316439535ae89fc9e1bb23f573c47e748
nowaveosu <nowaveosu@gmail.com>
2023-03-27 19:52:13 +0900

    3.27

--- 2023_03/로그인성공?.js
+++
@@ -1,7 +0,0 @@
-function solution(id_pw, db) {
-    //아이디,비번 일치하면 login리턴
-    const [id, pw] = id_pw;
-    const map = new Map(db);
-    return map.has(id) ? (map.get(id) === pw ? 'login' : 'wrong pw') : 'fail';
-  }
-  
