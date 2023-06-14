4b0bd11d8a64fda7fefa9aec208116c87aaa817c
nowaveosu <nowaveosu@gmail.com>
2023-06-14 21:38:03 +0900

    [Add] 6.14 알고리즘

---
+++ 2023_06/시저암호.js
@@ -0,0 +1,37 @@
+function solution(s, n) {
+    //[A,B...Z][a,b...z] includes 원소라면 +1
+    let Big = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    let small = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    let result = []
+    let arr = s.split('')
+    for(let el of arr){
+        if(Big.indexOf(el)>=0){
+            result.push(Big[Big.indexOf(el)+n]);
+        }
+        else if(small.indexOf(el)>=0){
+            result.push(small[small.indexOf(el)+n]);
+        }
+        else result.push(el);
+    }
+    return result.join('')
+}
+
+//다른풀이
+function solution(s, n) {
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lower = "abcdefghijklmnopqrstuvwxyz";
+    var answer= '';
+
+    for(var i =0; i <s.length; i++){
+        var text = s[i];
+        if(text == ' ') {
+            answer += ' '; 
+            continue;
+        }
+        var textArr = upper.includes(text) ? upper : lower;
+        var index = textArr.indexOf(text)+n;
+        if(index >= textArr.length) index -= textArr.length;
+        answer += textArr[index];
+    }
+    return answer;
+}
