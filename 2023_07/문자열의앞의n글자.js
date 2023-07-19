dc6a85a21e1395878a109cacc3064711cbf38740
bammuri <bammuriosu@gmail.com>
2023-07-19 23:33:06 +0900

    [Add] 7.19 알고리즘

---
+++ 2023_07/문자열의앞의n글자.js
@@ -0,0 +1,10 @@
+function solution(my_string, n) {
+    let string2 = my_string.slice(0,n);
+    return string2
+}
+
+//다른풀이
+
+function solution(my_string, n) {
+    return my_string.substring(0, n);
+}
