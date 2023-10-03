f253bf549a62ffdd7c682bb97b076bae8abdc03a
bammuri <bammuriosu@gmail.com>
2023-10-03 21:55:15 +0900

    [Add] 23.10.3 알고리즘

---
+++ 2023_10/공배수.js
@@ -0,0 +1,9 @@
+function solution(number, n, m) {
+    return (number % n === 0) && (number % m === 0) ? 1 : 0
+}
+
+//다른풀이
+
+function solution(number, n, m) {
+    return +!(number % n || number % m);
+}
