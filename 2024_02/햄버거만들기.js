f393b49dd928f9704e2cc64fab3149e835ae1845
nowaveosu <nowaveosu@gmail.com>
2024-02-01 17:22:26 +0900

    [Add] 24.2.1 알고리즘 다른풀이 추가

--- 2024_02/햄버거만들기.js
+++ 2024_02/햄버거만들기.js
@@ -10,3 +10,19 @@ function solution(ingredient) {
     }
     return count
 }
+
+//다른풀이
+function solution(ingredient) {
+    let answer = 0;
+    const stack = [];
+  
+    for (let i of ingredient) {
+      stack.push(i);
+  
+      if (stack.slice(-4).join('') == '1231') {
+        stack.splice(-4);
+        answer++;
+      }
+    }
+    return answer;
+  }
