a699664c3ae366818da33c61d97a7ab253b7504f
nowaveosu <nowaveosu@gmail.com>
2024-02-13 22:03:27 +0900

    [fix] 재귀 주석 추가

--- 2024_02/콜라츠수열.js
+++ 2024_02/콜라츠수열.js
@@ -13,9 +13,11 @@ function solution(n) {
     return answer;
 }
 
+//다른풀이
+
 function solution(n, arr = []) {
-    arr.push(n)
-    if (n === 1) return arr
-    if (n % 2 === 0) return solution(n / 2, arr)
-    return solution(3 * n + 1, arr)
+    arr.push(n); //arr에 n 추가
+    if (n === 1) return arr   //1개까지 줄여졌으면 리턴
+    if (n % 2 === 0) return solution(n / 2, arr) //짝수인경우 재귀
+    return solution(3 * n + 1, arr) //홀수인경우 재귀
 }
