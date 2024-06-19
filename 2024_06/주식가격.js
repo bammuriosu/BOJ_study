150b4a21b6dffa5ee2ffa3c810b6bb6f9fc50f96
nowaveosu <nowaveosu@gmail.com>
2024-06-19 21:40:38 +0900

    [Add] 6.19 알고리즘

--- 2024_06/주식가격.js
+++ 2024_06/주식가격.js
@@ -12,3 +12,31 @@ function solution(prices) {
     }
     return answer;
 }
+
+//다른풀이
+
+function solution(prices) {
+    // 가격이 떨어지지 않은 기간을 저장할 배열
+    const answer = new Array(prices.length).fill(0);
+    // 스택을 사용하여 가격이 떨어지지 않은 기간의 시작 인덱스를 저장
+    const stack = [];
+
+    for (let i = 0; i < prices.length; i++) {
+        // 스택이 비어있지 않고, 현재 가격이 이전 가격보다 작을 때
+        while (stack.length > 0 && prices[i] < prices[stack[stack.length - 1]]) {
+            // 스택에서 인덱스를 꺼내 해당 기간을 계산
+            const top = stack.pop();
+            answer[top] = i - top;
+        }
+        // 현재 인덱스를 스택에 추가
+        stack.push(i);
+    }
+
+    // 스택에 남아있는 인덱스 처리
+    while (stack.length > 0) {
+        const top = stack.pop();
+        answer[top] = prices.length - 1 - top;
+    }
+
+    return answer;
+}
