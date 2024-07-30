301550034ba0cf83d22e2e27ca049bb2ebc371b8
bammuri <bammuriosu@gmail.com>
2024-07-30 16:59:30 +0900

    [Add] 주석추가

--- 2024_07/sorted.py
+++ 2024_07/sorted.py
@@ -1,3 +1,3 @@
 my_list = [3, 2, 1, 4, 5]
-print(sorted(my_list))  # 오름차순 정렬
-print(sorted(my_list, reverse=True))  # 내림차순 정렬
+print(sorted(my_list))  # 오름차순 정렬 1 2 3 4 5
+print(sorted(my_list, reverse=True))  # 내림차순 정렬 5 4 3 2 1
